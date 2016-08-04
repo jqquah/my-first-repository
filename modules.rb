9e1087aa54148e617659679e79c7e25f0fcc208b
Wilson Quah <jqquah@gmail.com>
2016-08-04 23:39:07 +0800

    Wow, more already?

---
+++ modules.rb
@@ -0,0 +1,29 @@
+module Walkable
+  def walk
+    "I'm walking."
+  end
+end
+
+module Swimmable
+  def swim
+    "I'm swimming."
+  end
+end
+
+module Climbable
+  def climb
+    "I'm climbing."
+  end
+end
+
+class Animal
+  include Walkable
+
+  def speak
+    "I'm an animal, and I speak!"
+  end
+end
+
+fido = Animal.new
+fido.speak
+fido.walk
